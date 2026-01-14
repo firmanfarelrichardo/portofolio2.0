cef6b481d4ec6f018456f72781503c84b6334ceb
firmanfarelrichardo <farelpasaribu04@gmail.com>
2026-01-14 22:42:01 +0700

    test: husky verification round 2

--- portfolio/app/(public)/page.tsx
+++ portfolio/app/(public)/page.tsx
@@ -1,4 +1,4 @@
 export default function Home() {
-  const test: number = "Bukan Angka"; // ❌ Error ini harus tertangkap
+  const test: number = "Bukan Angka"; // ❌ Error ini harus tertangkapppp
   return <h1>Sistem Portofolio Firman Ready.</h1>;
 }
